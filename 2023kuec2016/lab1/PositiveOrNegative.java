a6661498798cc921fa4dfd53799823d9425a7d22
SKjustSK <sarthaksinghtariyal@gmail.com>
2025-01-09 12:23:34 +0530

    feat: Zero detection in PositiveOrNegativr.java

--- 2023kuec2016/lab1/PositiveOrNegative.java
+++ 2023kuec2016/lab1/PositiveOrNegative.java
@@ -5,7 +5,7 @@ class PositiveOrNegative {
 		System.out.print("Enter number: ");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		String ans = a >= 0 ? "Positive" : "Negative";
+		String ans = a >= 0 ? ((a == 0) ? "Zero" :"Positive") : "Negative";
 		System.out.println(ans);
 	}
 }
